240e09ec1dbea565b62c2c0f88b64c416d74ad01
Roman <obnov2012@yandex.ru>
2023-05-18 12:53:08 +0300

    first commit

---
+++ JS/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + ' arr' + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(decr--);
+
